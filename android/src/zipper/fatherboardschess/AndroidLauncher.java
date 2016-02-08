a93b21c46e90a2ab7bc90d0ef2599d013ceea404
fatherboards <pinecrestilab@gmail.com>
2016-02-08 15:09:39 -0500

    first commit

---
+++ android/src/zipper/fatherboardschess/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package zipper.fatherboardschess;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import zipper.fatherboardschess.FatherboardsChess;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FatherboardsChess(), config);
+	}
+}
